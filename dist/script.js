19b75939eef5b33b2ba424d312b72e73479db86b
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-24 02:36:23 +0600

    coming soon 2.0 updated

---
+++ dist/script.js
@@ -0,0 +1,35 @@
+// Reverse countdown timer
+const startDate = new Date("Aug 21, 2025 00:00:00").getTime();
+const x = setInterval(() => {
+  // Countdown DOM elements
+  const showDays = document.getElementById("days");
+  const showHours = document.getElementById("hours");
+  const showMinutes = document.getElementById("minute");
+  const showSeconds = document.getElementById("second");
+
+  // get new date
+  const currentTime = new Date().getTime();
+  const distance = startDate - currentTime;
+
+  // calculate time
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // add DOM
+  showDays.innerHTML = days;
+  showHours.innerHTML = hours < 10 ? "0" + hours : hours;
+  showMinutes.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+  showSeconds.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    showDays.innerHTML = "00";
+    showHours.innerHTML = "00";
+    showMinutes.innerHTML = "00";
+    showSeconds.innerHTML = "00";
+  }
+}, 1000);
